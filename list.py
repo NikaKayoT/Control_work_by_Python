920d0bbcc12b414674984bde628c1d725001830f
Nika <nika.berner@mail.ru>
2024-05-13 13:04:25 +0300

    add list

---
+++ list.py
@@ -0,0 +1,9 @@
+import csv
+def list_notes():
+    notes = []
+    with open('notes.csv', 'r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for index, row in enumerate(reader):
+            print(f"{index + 1}. Дата: {row[0]}, Заголовок: {row[1]}, Сообщение: {row[2]}")
+            notes.append(row)
+    return notes
